4964b2ad98639124fa155004676c30dad48c56ef
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2019-01-19 13:51:49 -0800

    Viewfinder
    Box on screen now represents the viewfinder

--- app/src/main/java/wenjalan/mangatranslator/MainActivity.java
+++ app/src/main/java/wenjalan/mangatranslator/MainActivity.java
@@ -1,52 +1,101 @@
 package wenjalan.mangatranslator;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+    private static final String TAG = "StrikeZone";
+    private CameraBridgeViewBase mOpenCvCameraView;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV initialized successfully");
         }
-
-        return super.onOptionsItemSelected(item);
+        else {
+            Log.d(TAG, "OpenCV failed to initialize");
+        }
     }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        // OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_4_1_0, this, mLoaderCallback);
+        OpenCVLoader.initDebug();
+        mOpenCvCameraView.enableView();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.MainCameraView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        // turn on the camera
+        mOpenCvCameraView.enableView();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    Mat mRgba;
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Log.d(TAG, "Got a frame");
+        mRgba = inputFrame.rgba();
+
+        int viewFinderHeight = 300;
+        int viewFinderWidth = 500;
+
+        int height = mRgba.height();
+        int width = mRgba.width();
+
+        int corner1 = width / 2 - (viewFinderWidth / 2);
+        int corner2 = height / 2 - (viewFinderHeight / 2);
+
+        Rect rect = new Rect(corner1, corner2, viewFinderWidth, viewFinderHeight);
+
+        // white
+        Scalar color = new Scalar(255, 255, 255, 100);
+
+        Imgproc.rectangle(mRgba, rect, color, 5);
+
+        // return the mat
+        return mRgba;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {}
+
+    @Override
+    public void onCameraViewStopped() { }
+
 }

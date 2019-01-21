b11fac18c47de3bd1285cd4efb70f72c464802fe
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2019-01-20 17:32:04 -0800

    Restored CloudVisionOCR Build

--- app/src/main/java/wenjalan/mangatranslator/TranslationManager.java
+++ app/src/main/java/wenjalan/mangatranslator/TranslationManager.java
@@ -1,27 +1,13 @@
 package wenjalan.mangatranslator;
 
-import com.google.cloud.translate.Translate;
-import com.google.cloud.translate.TranslateOptions;
-
-import static wenjalan.mangatranslator.Key.GOOGLE_API_KEY;
-
 public class TranslationManager {
 
-    private static Translate translate;
+    public TranslationManager() {
 
-    public static Translate getTranslate() {
-        TranslateOptions options = TranslateOptions.newBuilder()
-                .setApiKey(GOOGLE_API_KEY)
-                .build();
-        return options.getService();
     }
 
-    // translates a string of text (japanese) into english
     public static String translate(String text) {
-        if (translate == null) {
-            translate = getTranslate();
-        }
-        return "hello";
+        return null;
     }
 
 }

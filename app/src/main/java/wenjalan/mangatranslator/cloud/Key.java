4a567ac48081e0c2bb15524d030fec695dd4e7c2
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2019-05-27 21:30:00 -0700

    Update Key.java
    removed the key from source code

--- app/src/main/java/wenjalan/mangatranslator/cloud/Key.java
+++ app/src/main/java/wenjalan/mangatranslator/cloud/Key.java
@@ -2,7 +2,8 @@ package wenjalan.mangatranslator.cloud;
 
 public class Key {
 
-    public static final String GOOGLE_API_KEY = "AIzaSyCHm_E5aK3YXOxkiLuFtOMzpqGSJu0glNo";
+    // do not do this
+    public static final String GOOGLE_API_KEY = "...";
 
 
 }
